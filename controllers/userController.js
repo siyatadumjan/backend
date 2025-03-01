3925b7b5d3eb2b24b20c81af9968b547a024657a
siyatadumjan <135720118+siyatadumjan@users.noreply.github.com>
2025-03-01 16:54:28 +0545

    .

--- controllers/userController.js
+++ controllers/userController.js
@@ -76,7 +76,7 @@ export const login = async (req, res) => {
 }
 export const logout = (req, res) => {
     try {
-        return res.status(200).cookie("token", "", { maxAge: 0 })json({
+        return res.status(200).cookie("token", "", { maxAge: 0 }).json({
             message: "logged out successfully."
         })
     } catch (error) {
